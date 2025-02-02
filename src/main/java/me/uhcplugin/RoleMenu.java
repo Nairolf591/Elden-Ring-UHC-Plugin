9070dfee949c3133cbed1b7ff744212bd9ae5347
Nairolf591 <florianp59580@gmail.com>
2025-02-02 19:30:54 +0100

    Update RoleMenu.java

--- src/main/java/me/uhcplugin/RoleMenu.java
+++ src/main/java/me/uhcplugin/RoleMenu.java
@@ -84,7 +84,8 @@ public class RoleMenu implements Listener {
 
         // Gestion du bouton "Retour"
         if (clickedItem.getType() == Material.ARROW) {
-            openConfigMenu(player);
+                Main.getInstance().openConfigMenu(player);  // Correct si Main a une instance statique
+
             return;
         }
 
